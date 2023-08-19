93dbca277c870b9cf38f3e734bc4e63cff614957
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-19 10:15:08 +0530

    Add files via upload

---
+++ Function1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void student_info();
+int main(){
+    student_info();
+    return 0;
+}
+void student_info()
+{
+    printf("Name: Dependra Banet");
+    printf("\nRoll No: 2213986060");
+    printf("\nCourse:BCA");
+}
